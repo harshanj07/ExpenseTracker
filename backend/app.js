67ebcba14f730d57586a321dcb13234d7c02ba2f
harshanj07 <jharshan07@gmail.com>
2025-03-14 23:23:34 +0530

    commit

---
+++ backend/app.js
@@ -0,0 +1,40 @@
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./DB/Database.js";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import transactionRoutes from "./Routers/Transactions.js";
+import userRoutes from "./Routers/userRouter.js";
+
+dotenv.config({ path: "./config/.env" });
+const app = express();
+
+const PORT = process.env.PORT || 5000; // Default to port 5000 if env variable is not set
+
+connectDB();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Routes
+app.use("/api/v1", transactionRoutes);
+app.use("/api/auth", userRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// Start Server
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+});
+
+export default app;
